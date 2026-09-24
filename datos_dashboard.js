0f94c6237bc630a73d4ff0a1567479a2435569a9
mdfaure1969 <miguel.faure@credencial.com.ar>
2026-09-24 13:28:07 -0300

    Dashboard actualizado 2026-09-24

--- datos_dashboard.js
+++ datos_dashboard.js
@@ -1,6 +1,6 @@
 window.DASHBOARD_CONTROL_SALDOS = {
   "metadata": {
-    "generado_en": "2026-09-24 13:22:10",
+    "generado_en": "2026-09-24 13:28:06",
     "archivo_origen": "C:\\Users\\mfaure\\OneDrive - Credencial Argentina S. A\\CDP – Controles Diarios\\Control Saldos\\02_Maestros\\historico_control_saldos.xlsx",
     "carpeta_origen": "C:\\Users\\mfaure\\OneDrive - Credencial Argentina S. A\\CDP – Controles Diarios\\Control Saldos\\02_Maestros",
     "ultima_fecha_datos": "2026-09-24",
